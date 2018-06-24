589b721fbaa47eb8d9891795e8132b1a25e938ec
shrey <shreymisra8@gmail.com>
2018-06-24 23:15:37 +0530

    fragment 2 done

--- app/src/main/java/org/shreya/project/sample/Fragments/Fragment1.java
+++ app/src/main/java/org/shreya/project/sample/Fragments/Fragment1.java
@@ -24,6 +24,8 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
+        
         return inflater.inflate(R.layout.fragment_fragment1, container, false);
     }
 
